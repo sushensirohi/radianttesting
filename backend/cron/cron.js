5b5135f01d89797bffb22bbf93ba92fbe98ef7d5
shanesirohi <shanesirohi@gmail.com>
2024-06-17 06:46:20 +0530

    first commit

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -1,7 +1,7 @@
 import cron from "cron";
 import https from "https";
 
-const URL = "https://radianttesting.onrender.com";
+const URL = "https://radiant.onrender.com";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
 	https
